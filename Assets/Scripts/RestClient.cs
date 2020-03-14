2cb334c85c7f6c44c75bb27dafb18015a6e7ff4b
Ted Moseley <tmoseley1106@gmail.com>
2020-03-14 11:56:28 -0400

    Initial commit with basic GET and POST

---
+++ Assets/Scripts/RestClient.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+public class RestClient
+{
+    public static async Task<string> GetJsonString(string url)
+    {
+        using (var httpClient = new HttpClient())
+        {
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await httpClient.GetAsync(url);
+
+            if (response.StatusCode != HttpStatusCode.OK)
+                return null;
+
+            string resourceString = await response.Content.ReadAsStringAsync();
+
+            return resourceString;
+        }
+    }
+
+    public static async Task<string> PostJsonString<T>(string url, T payload)
+    {
+        string payloadString = JsonConvert.SerializeObject(payload);
+
+        var content = new StringContent(payloadString, Encoding.UTF8, "application/json");
+
+        using (var httpClient = new HttpClient())
+        {
+            var response = await httpClient.PostAsync(url, content);
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ Assets/Scripts/UiBehavior.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UiBehavior : MonoBehaviour
+{
+    public Button requestButton;
+
+    public TMP_Text messageText;
+
+    private Dictionary<string, Action> functionMap;
+
+    public async void getClick()
+    {
+        var myGetString = await RestClient.GetJsonString("https://jsonplaceholder.typicode.com/todos/1");
+
+        messageText.text = myGetString;
+    }
+
+    public async void postClick()
+    {
+        var postPayload = new Dictionary<string, object>();
+
+        postPayload.Add("title", "foo");
+        postPayload.Add("body", "bar");
+        postPayload.Add("userId", 1);
+
+        var myPostString = await RestClient.PostJsonString(
+            "https://jsonplaceholder.typicode.com/posts", postPayload);
+
+        messageText.text = myPostString;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
